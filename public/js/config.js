4e67314a716a0e50276239940f786cda6a37dfd7
AleemShalu <106386487+AleemShalu@users.noreply.github.com>
2025-12-01 19:24:01 +0530

    second commit

--- public/js/config.js
+++ public/js/config.js
@@ -1,9 +1,8 @@
 // Application Configuration
 // Change this URL for production deployment
 window.APP_CONFIG = {
-  API_BASE_URL: "http://localhost:3000/api",
-  BASE_URL: "http://localhost:3000", // Base URL for file/image paths
+  // API_BASE_URL: "http://localhost:3000/api",
+  // BASE_URL: "http://localhost:3000", // Base URL for file/image paths
+  API_BASE_URL: "https://apig1.fairliefer.de/api",
+  BASE_URL: "https://apig1.fairliefer.de", // Base URL for file/image paths
 };
-
-
-
